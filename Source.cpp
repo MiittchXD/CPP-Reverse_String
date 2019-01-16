80926e338f4c522d3697198756357ea537584653
Mitchell Bartoszyk <mbartoszyk@gmail.com>
2019-01-15 21:53:42 -0500

    First Commit

---
+++ Source.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include<iostream>
+
+using namespace std;
+
+
+
+string reverseString(string original) {
+	string reverse = original;
+	char temp;
+	int endIndex = reverse.length() - 1;
+
+	if (reverse.length() % 2 == 0) {
+		for (int i = 0; i < reverse.length() / 2; i++) {
+			temp = reverse.at(i);
+			reverse.at(i) = reverse.at(endIndex);
+			reverse.at(endIndex) = temp;
+			endIndex--;
+		}
+	}
+	else {
+		for (int i = 0; i < reverse.length() / 2 - 1; i++) {
+			temp = reverse.at(i);
+			reverse.at(i) = reverse.at(endIndex);
+			reverse.at(endIndex) = temp;
+			endIndex--;
+		}
+	}
+	return reverse;
+}
+
+int main() {
+	string myString = "Seven";
+	string reverseStr = reverseString(myString);
+	cout << reverseStr;
+
+
+
+	system("pause");
+	return 0;
+}
