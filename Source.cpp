ec6254e1b81c2c1a74601b760ade53148137cdc4
Mitchell Bartoszyk <mbartoszyk@gmail.com>
2019-01-15 21:56:46 -0500

    Added comments for readability

--- Source.cpp
+++ Source.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-
-
 string reverseString(string original) {
 	string reverse = original;
 	char temp;
 	int endIndex = reverse.length() - 1;
 
+	// String is of even length
 	if (reverse.length() % 2 == 0) {
 		for (int i = 0; i < reverse.length() / 2; i++) {
 			temp = reverse.at(i);
@@ -18,7 +17,7 @@ string reverseString(string original) {
 			endIndex--;
 		}
 	}
-	else {
+	else { // String is of odd length
 		for (int i = 0; i < reverse.length() / 2 - 1; i++) {
 			temp = reverse.at(i);
 			reverse.at(i) = reverse.at(endIndex);
@@ -33,8 +32,6 @@ int main() {
 	string myString = "Seven";
 	string reverseStr = reverseString(myString);
 	cout << reverseStr;
-
-
 
 	system("pause");
 	return 0;
